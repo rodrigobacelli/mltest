1add3875594c212f098efec5109f8cef7b5e0a8c
Rodrigo Bacelli <rodrigobac@gmail.com>
2016-12-04 17:07:43 -0200

    Add MercadoLivre eslint rules

---
+++ config/eslint/globals.js
@@ -0,0 +1,15 @@
+module.exports = {
+    globals: {
+        $: true,
+        _: true,
+        ch: true,
+        tiny: true,
+        App: true,
+        Handlebars: true,
+        Mercadopago: true,
+
+        // i18n
+        gettext: true,
+        ngettext: true
+    }
+};
