2d68922bc321409d34f7f4561e80ef9a718e6188
Rodrigo Bacelli <rodrigobac@gmail.com>
2016-12-04 18:12:09 -0200

    Move html to index file to be minifyed later

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 var express = require("express"),
     app     = express()
 
-app.get("/", function (req, res) {
-    res.send("<!DOCTYPE html>" +
-        "<html>" +
-        "<head>" +
-        "<title>ML Test</title>" +
-        "</head>" +
-        "<body>" +
-        "<div id='app'></div>" +
-        "<script type='text/javascript' src='/static/bundle.js'></script>" +
-        "</body>" +
-        "</html>")
-})
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/src/index.html')
+});
 
 app.get("/static/bundle.js", function (req, res) {
     res.sendFile("/static/bundle.js", {root: __dirname})
