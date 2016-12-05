ccf4ba44c6ac940a266038555276556b875d7e79
Rodrigo Bacelli <rodrigobac@gmail.com>
2016-12-04 22:57:39 -0200

    Add initial codes for Product Page

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Zoom } from './react-chico';
 
 export default class App extends React.Component {
     render() {
         return (
-            <div />
+            <article itemScope itemType="http://schema.org/Product">
+                <h1 itemProp="name">Ipad Mini Apple 16 Gb Wifi Led 7.9</h1>
+                <Zoom
+                    image="assets/img/products/ipad.jpg"
+                    imageBig="assets/img/products/ipad_big.jpg"
+                />
+            </article>
         );
     }
 }
