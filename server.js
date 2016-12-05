6c745477b86da650d4c00fb9942a85c961dd58f1
Rodrigo Bacelli <rodrigobac@gmail.com>
2016-12-04 22:54:59 -0200

    Add file serving for assets and css images to server

--- server.js
+++ server.js
@@ -9,4 +9,21 @@ app.get("/static/bundle.js", function (req, res) {
     res.sendFile("/static/bundle.js", {root: __dirname})
 })
 
+app.get("/static/css/main.css", function (req, res) {
+    res.sendFile("/static/css/main.css", {root: __dirname})
+})
+
+app.get("/assets/**/*.*", function (req, res) {
+    var path = req.params[0] ? req.params[0] : 'index.html',
+        file = req.params[2] && req.params[3] ? req.params[2] + '.' + req.params[3] : 'index.html';
+
+    var path_name = '';
+    if (path === 'index.html') {
+        path_name = '/assets/index.html'
+    } else {
+        path_name = '/assets/'+ path + '/' + file;
+    }
+    res.sendFile(path_name, {root: __dirname})
+})
+
 app.listen(3000)
